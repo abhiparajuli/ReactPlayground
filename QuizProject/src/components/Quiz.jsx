e75f38e8255600cc6525297f233ea837be8ab3c6
A Parajuli <aparajuli@xylontech.com>
2025-03-14 22:56:14 +0545

    Modularize by creating separate components. UseRef to avoid unnecessary useEffect

--- QuizProject/src/components/Quiz.jsx
+++ QuizProject/src/components/Quiz.jsx
@@ -1,68 +1,46 @@
-import { useState, useCallback } from "react";
+import { useState, useCallback } from 'react';
 
 import QUESTIONS from '../questions.js';
-import QuestionTimer from "./QuestionTImer.jsx";
 import quizCompleteImg from '../assets/quiz-complete.png';
+import Question from './Question.jsx';
 
 export default function Quiz() {
- const [answerState, setAnswerState] = useState('');
- const [userAnswers, setUserAnswers] = useState([]);
+  const [userAnswers, setUserAnswers] = useState([]);
 
- const activeQuestionIndex = answerState === '' ? userAnswers.length : userAnswers.length - 1;
- const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
+  const activeQuestionIndex = userAnswers.length;
+  const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
 
- const handleSelectAnswer = useCallback(function handleSelectAnswer(selectedAnswer) {
-    setAnswerState('answered');
-    setUserAnswers((prevAnswers) => {
-        return [...prevAnswers, selectedAnswer]
+  const handleSelectAnswer = useCallback(function handleSelectAnswer(
+    selectedAnswer
+  ) {
+    setUserAnswers((prevUserAnswers) => {
+      return [...prevUserAnswers, selectedAnswer];
     });
+  },
+  []);
 
-    setTimeout(() => {
-        if (selectedAnswer === QUESTIONS[activeQuestionIndex].answers[0]) {
-            setAnswerState('correct');
-        } else {
-            setAnswerState('incorrect');
-        }
+  const handleSkipAnswer = useCallback(
+    () => handleSelectAnswer(null),
+    [handleSelectAnswer]
+  );
 
-        setTimeout(() => {
-            setAnswerState('');
-        }, 2000);
-    }, 1000);
- }, 
- [activeQuestionIndex]
-);
-
- const handleSkipAnswer = useCallback(() => handleSelectAnswer(null), [handleSelectAnswer]);
-
- if (quizIsComplete) {
+  if (quizIsComplete) {
     return (
-        <div id="summary">
-            <img src={quizCompleteImg} alt="Trophy Icon" />
-            <h2>Quiz Complete!</h2>
-        </div>
+      <div id="summary">
+        <img src={quizCompleteImg} alt="Trophy icon" />
+        <h2>Quiz Completed!</h2>
+      </div>
     );
- }
+  }
 
- const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers];
- shuffledAnswers.sort(() => Math.random() - 0.5);
-
- return (
+  return (
     <div id="quiz">
-        <div id="question">
-            <QuestionTimer 
-                key={activeQuestionIndex}
-                timeout={15000} 
-                onTimeout={handleSkipAnswer} 
-            />
-            <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
-            <ul id="answers">
-                {shuffledAnswers.map((answer) => (
-                    <li key={answer} className="answer">
-                        <button onClick={() => handleSelectAnswer(answer)}>{answer}</button>
-                    </li>
-                ))}
-            </ul>
-        </div>
+      <Question
+        key={activeQuestionIndex}
+        index={activeQuestionIndex}
+        onSelectAnswer={handleSelectAnswer}
+        onSkipAnswer={handleSkipAnswer}
+      />
     </div>
- );
+  );
 }
